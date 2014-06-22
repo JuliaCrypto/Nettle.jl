3f60dd523bdb89e2b7505352fcbe443079e4fc71
Elliot Saba <staticfloat@gmail.com>
2014-06-21 23:35:05 -0700

    Update install_julia_osx.sh
    Totally had `nightlies` and `releases` swapped here.

--- CI/install_julia_osx.sh
+++ CI/install_julia_osx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Install julia into ~/julia on OSX
-if [[ "$JULIAVERSION" == "releases" ]]; then
+if [[ "$JULIAVERSION" == "nightlies" ]]; then
     wget -O julia.dmg http://status.julialang.org/download/osx10.7+
 fi
-if [[ "$JULIAVERSION" == "nightlies" ]]; then
+if [[ "$JULIAVERSION" == "releases" ]]; then
     wget -O julia.dmg http://status.julialang.org/stable/osx10.7+
 fi
 
