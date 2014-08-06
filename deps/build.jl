b0a3449d48ba0b1b6af37d2c0d1dd7f6ec270087
Elliot Saba <staticfloat@gmail.com>
2014-08-06 15:34:52 -0400

    Add SHA hash for nettle tarball

--- deps/build.jl
+++ deps/build.jl
@@ -24,7 +24,10 @@ env = {"HOGWEED_LIBS" => "-L$(libdirs[1]) -L$(BinDeps.libdir(nettle)) -lhogweed 
        "NETTLE_LIBS" => "-L$(libdirs[1]) -L$(BinDeps.libdir(nettle)) -lnettle -lgmp",
        "LIBS" => "-lgmp ", "LD_LIBRARY_PATH" => join([libdirs[1];BinDeps.libdir(nettle)],":")}
 
-provides( Sources, URI("http://www.lysator.liu.se/~nisse/archive/nettle-2.7.1.tar.gz"), nettle )
+provides( Sources,
+          URI("http://www.lysator.liu.se/~nisse/archive/nettle-2.7.1.tar.gz"),
+          SHA="297c69e90bbd448f72e854abe5cc7868c08d710e1c1bcd6a14adf06e25629d58a3ef4d65ab588d001ec7091aa583032312ad15b416ea5479e5bf0ea63717f473",
+          nettle )
 provides( BuildProcess,
           Autotools(lib_dirs = libdirs,
                     include_dirs = includedirs,
