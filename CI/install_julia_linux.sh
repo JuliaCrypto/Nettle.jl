5dd19c44ce2e65ce7569b2d4f230917660bb7eae
Elliot Saba <staticfloat@gmail.com>
2015-02-17 14:25:07 -0800

    Modernize CI, try adding Coveralls

--- CI/install_julia_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo add-apt-repository ppa:staticfloat/julia-deps -y
-sudo add-apt-repository ppa:staticfloat/julia${JULIAVERSION} -y
-sudo apt-get update -qq -y
-sudo apt-get install libpcre3-dev julia -y
