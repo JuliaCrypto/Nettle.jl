bcd4f273316886db498aedce052145ad848c00bb
Tony Kelman <tony@kelman.net>
2016-02-09 09:25:31 -0800

    add yet a new alias

--- deps/build.jl
+++ deps/build.jl
@@ -3,11 +3,11 @@ using Compat
 
 @BinDeps.setup
 
-nettle = library_dependency("nettle", aliases = ["libnettle","libnettle-4-6","libnettle-6-1"])
+nettle = library_dependency("nettle", aliases = ["libnettle","libnettle-4-6","libnettle-6-1","libnettle-6-2"])
 
 @windows_only begin
   using WinRPM
-  provides(WinRPM.RPM, "libnettle-6-1", nettle, os = :Windows )
+  provides(WinRPM.RPM, "libnettle-6-2", nettle, os = :Windows )
 end
 
 @osx_only begin
