c2a28298765890eb33ed13a13aa7b8abfc096847
Lyndon White <oxinabox@ucc.asn.au>
2018-01-15 21:28:46 +0800

    minor refactor of build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -12,28 +12,28 @@ end
 
 if is_apple()
   using Homebrew
-  provides( Homebrew.HB, "nettle", nettle, os = :Darwin )
+  provides(Homebrew.HB, "nettle", nettle, os = :Darwin )
 end
 
-provides( AptGet, "libnettle4", nettle )
-provides( Yum, "nettle", nettle )
+provides(AptGet, "libnettle4", nettle )
+provides(Yum, "nettle", nettle )
 
 julia_usrdir = normpath(JULIA_HOME*"/../") # This is a stopgap, we need a better built-in solution to get the included libraries
-libdirs = AbstractString["$(julia_usrdir)/lib"]
-includedirs = AbstractString["$(julia_usrdir)/include"]
+libdirs = ["$(julia_usrdir)/lib"]
+includedirs = ["$(julia_usrdir)/include"]
 env = @compat Dict("HOGWEED_LIBS" => "-L$(libdirs[1]) -L$(BinDeps.libdir(nettle)) -lhogweed -lgmp",
        "NETTLE_LIBS" => "-L$(libdirs[1]) -L$(BinDeps.libdir(nettle)) -lnettle -lgmp",
        "LD_LIBRARY_PATH" => join([libdirs[1];BinDeps.libdir(nettle);get(ENV,"LD_LIBRARY_PATH","")],":"))
 
-provides( Sources,
-          URI("http://www.lysator.liu.se/~nisse/archive/nettle-2.7.1.tar.gz"),
-          SHA="bc71ebd43435537d767799e414fce88e521b7278d48c860651216e1fc6555b40",
-          nettle )
-provides( BuildProcess,
-          Autotools(lib_dirs = libdirs,
-                    include_dirs = includedirs,
-                    env = env,
-                    configure_options = ["--disable-openssl", "--libdir=$(BinDeps.libdir(nettle))"]),
-          nettle )
+provides(Sources,
+         URI("http://www.lysator.liu.se/~nisse/archive/nettle-2.7.1.tar.gz"),
+         SHA="bc71ebd43435537d767799e414fce88e521b7278d48c860651216e1fc6555b40",
+         nettle)
+provides(BuildProcess,
+         Autotools(lib_dirs = libdirs,
+                   include_dirs = includedirs,
+                   env = env,
+                   configure_options = ["--disable-openssl", "--libdir=$(BinDeps.libdir(nettle))"]),
+                   nettle)
 
 @compat @BinDeps.install Dict(:nettle => :nettle)
