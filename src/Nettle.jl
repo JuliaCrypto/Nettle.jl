fec566a12a0fa581a836c8db43b270588e37da00
Elliot Saba <staticfloat@gmail.com>
2020-04-02 15:54:21 -0700

    Upgrade Nettle to Julia 1.3+
    * Use JLL package (death to BinaryProvider!)
    * Add Project.toml

--- src/Nettle.jl
+++ src/Nettle.jl
@@ -1,21 +1,10 @@
 module Nettle
-
-# Load libnettle from our deps.jl
-const depsjl_path = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
-if !isfile(depsjl_path)
-    error("Nettle not installed properly, run Pkg.build(\"Nettle\"), restart Julia and try again")
-end
-include(depsjl_path)
+using Nettle_jll, Libdl
 
 include("hash_common.jl")
 include("hash.jl")
 include("hmac.jl")
 include("cipher.jl")
-
-function __init__()
-    # Always check your dependencies that live in `deps.jl`
-    check_deps()
-end
 
 # SnoopCompile acceleration
 include("precompile.jl")
